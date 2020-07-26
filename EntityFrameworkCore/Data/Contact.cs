367dafbddf475ca5fb7c80b5a3bd83d7cce01085
Prasob kumar kp <prasobkumarkp@gmail.com>
2020-07-26 17:44:42 +0530

    * Contact.cs: * SqlLite.db-wal: * 20200726121206_AddedContactAdress.cs: * 20200726121055_AddFaultyMigrations.cs: * 20200726121206_AddedContactAdress.Designer.cs: * 20200726121055_AddFaultyMigrations.Designer.cs:
    * AppDbContextModelSnapshot.cs: Added contacts adress

--- EntityFrameworkCore/Data/Contact.cs
+++ EntityFrameworkCore/Data/Contact.cs
@@ -11,5 +11,6 @@ namespace EntityFrameworkCore.Data
         [Required]
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
+        public string  Address { get; set; }
     }
 }
---
+++ EntityFrameworkCore/Migrations/20200726121206_AddedContactAdress.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EntityFrameworkCore.Migrations
+{
+    public partial class AddedContactAdress : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "Contacts",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "Contacts");
+        }
+    }
+}
